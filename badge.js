2928e1e9d360358610f75dd019f2ce5fe24cabbc
Aravind V Nair <22199259+aravindvnair99@users.noreply.github.com>
2020-10-04 04:13:19 +0530

    Refactor
    Signed-off-by: Aravind V Nair <22199259+aravindvnair99@users.noreply.github.com>

--- badge.js
+++ badge.js
@@ -1,9 +1,9 @@
 let img;
 let input;
 function setup() {
-  createCanvas(400, 300);
-  img = loadImage("assets/logo.png");
-  input = createFileInput(handleFile);
+	createCanvas(400, 300);
+	img = loadImage("assets/logo.png");
+	input = createFileInput(handleFile);
 }
 // const fileSelector = document.getElementById("file-selector");
 // fileSelector.addEventListener("change", (event) => {
@@ -15,25 +15,25 @@ function setup() {
 // });
 
 function handleFile(file) {
-  if (file.type === "image") {
-    img = createImg(file.data, "");
-    img.elt.id = "temp-img";
-    setTimeout(() => {
-      resizeCanvas(height * (img.width / img.height), height);
-    }, 50);
-    // img.hide();
-  } else {
-    img = null;
-  }
+	if (file.type === "image") {
+		img = createImg(file.data, "");
+		img.elt.id = "temp-img";
+		setTimeout(() => {
+			resizeCanvas(height * (img.width / img.height), height);
+		}, 50);
+		// img.hide();
+	} else {
+		img = null;
+	}
 }
 
 function draw() {
-  background(220);
-  image(img, 0, 0, height * (img.width / img.height), height);
+	background(220);
+	image(img, 0, 0, height * (img.width / img.height), height);
 }
 
 function asb() {
-  document.querySelectorAll("#temp-img").src = document
-    .querySelector("canvas")
-    .toDataURL();
+	document.querySelectorAll("#temp-img").src = document
+		.querySelector("canvas")
+		.toDataURL();
 }
--- main.js
+++ main.js
@@ -11,142 +11,141 @@ let Im;
 let img;
 let deltaX, deltaY;
 async function load() {
-  model = await blazeface.load();
-  status.innerHTML = "Model Loaded";
-  const fileSelector = document.getElementById("file-selector");
-  fileSelector.addEventListener("change", (event) => {
-    img = document.querySelector("#tp");
-    const file = event.target.files[0];
-    let src = URL.createObjectURL(file);
-    img.src = src;
+	model = await blazeface.load();
+	status.innerHTML = "Model Loaded";
+	const fileSelector = document.getElementById("file-selector");
+	fileSelector.addEventListener("change", (event) => {
+		img = document.querySelector("#tp");
+		const file = event.target.files[0];
+		let src = URL.createObjectURL(file);
+		img.src = src;
+		Im = new Image();
+		Im.onload = () => {
+			ctx.drawImage(
+				Im,
+				0,
+				0,
+				canvas.width,
+				canvas.width * (Im.height / Im.width)
+			);
+			img.src = canvas.toDataURL();
+			status.innerHTML = "Image Loaded";
+			setTimeout(() => {
+				run();
+			}, 500);
+		};
+		Im.src = src;
 
-    Im = new Image();
-    Im.onload = () => {
-      ctx.drawImage(
-        Im,
-        0,
-        0,
-        canvas.width,
-        canvas.width * (Im.height / Im.width)
-      );
-      img.src = canvas.toDataURL();
-      status.innerHTML = "Image Loaded";
-      setTimeout(() => {
-        run();
-      }, 500);
-    };
-    Im.src = src;
+		// weird
 
-    // weird
+		// Im = new Image();
+		// Im.onload = function () {
+		//   // set size proportional to image
+		//   canvas.height = canvas.width * (Im.height / Im.width);
 
-    // Im = new Image();
-    // Im.onload = function () {
-    //   // set size proportional to image
-    //   canvas.height = canvas.width * (Im.height / Im.width);
+		//   // step 1 - resize to 50%
+		//   var oc = document.createElement("canvas"),
+		//     octx = oc.getContext("2d");
 
-    //   // step 1 - resize to 50%
-    //   var oc = document.createElement("canvas"),
-    //     octx = oc.getContext("2d");
+		//   oc.width = Im.width * 0.5;
+		//   oc.height = Im.height * 0.5;
+		//   octx.drawImage(Im, 0, 0, oc.width, oc.height);
 
-    //   oc.width = Im.width * 0.5;
-    //   oc.height = Im.height * 0.5;
-    //   octx.drawImage(Im, 0, 0, oc.width, oc.height);
+		//   // step 2
+		//   octx.drawImage(oc, 0, 0, oc.width * 0.5, oc.height * 0.5);
 
-    //   // step 2
-    //   octx.drawImage(oc, 0, 0, oc.width * 0.5, oc.height * 0.5);
-
-    //   // step 3, resize to final size
-    //   ctx.drawImage(
-    //     oc,
-    //     0,
-    //     0,
-    //     oc.width * 0.5,
-    //     oc.height * 0.5,
-    //     0,
-    //     0,
-    //     canvas.width,
-    //     canvas.height
-    //   );
-    //   img.src = canvas.toDataURL();
-    //   status.innerHTML = "Image Loaded";
-    //   setTimeout(() => {
-    //     run();
-    //   }, 500);
-    // };
-    // Im.src = src;
-  });
+		//   // step 3, resize to final size
+		//   ctx.drawImage(
+		//     oc,
+		//     0,
+		//     0,
+		//     oc.width * 0.5,
+		//     oc.height * 0.5,
+		//     0,
+		//     0,
+		//     canvas.width,
+		//     canvas.height
+		//   );
+		//   img.src = canvas.toDataURL();
+		//   status.innerHTML = "Image Loaded";
+		//   setTimeout(() => {
+		//     run();
+		//   }, 500);
+		// };
+		// Im.src = src;
+	});
 }
 
 async function run() {
-  let res = await model.estimateFaces(document.getElementById("tp"), false);
+	let res = await model.estimateFaces(document.getElementById("tp"), false);
 
-  if (res.length == 0) {
-    status.innerHTML = "Can't find Your Face";
-    document.getElementById("manual-button").style.display = "block";
-  }
+	if (res.length == 0) {
+		status.innerHTML = "Can't find Your Face";
+		document.getElementById("manual-button").style.display = "block";
+	}
 
-  res = res[0];
+	res = res[0];
 
-  //   res.landmarks.forEach((pos) => {
-  //     ctx.beginPath();
-  //     ctx.arc(pos[0], pos[1], 10, 0, Math.PI * 2);
-  //     ctx.fillStyle = "red";
-  //     ctx.fill();
-  //   });
+	//   res.landmarks.forEach((pos) => {
+	//     ctx.beginPath();
+	//     ctx.arc(pos[0], pos[1], 10, 0, Math.PI * 2);
+	//     ctx.fillStyle = "red";
+	//     ctx.fill();
+	//   });
 
-  let faceCenterX = (res.topLeft[0] + res.bottomRight[0]) / 2;
-  let faceCenterY = (res.topLeft[1] + res.bottomRight[1]) / 2;
+	let faceCenterX = (res.topLeft[0] + res.bottomRight[0]) / 2;
+	let faceCenterY = (res.topLeft[1] + res.bottomRight[1]) / 2;
 
-  let width = res.bottomRight[0] - res.topLeft[0];
-  let height = res.bottomRight[1] - res.topLeft[1];
+	let width = res.bottomRight[0] - res.topLeft[0];
+	let height = res.bottomRight[1] - res.topLeft[1];
 
-  console.log(width * height);
-  status.innerHTML = "Adjusting";
-  deltaX = canvas.width / 2 - faceCenterX;
-  deltaY = canvas.height / 2 - faceCenterY;
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
-  // ctx.drawImage(bg, 0, 0, canvas.width, canvas.height * 1.05);
-  ctx.drawImage(
-    Im,
-    deltaX,
-    deltaY,
-    canvas.width,
-    canvas.width * (Im.height / Im.width)
-  );
-  ctx.drawImage(frame, 0, 0, canvas.width, canvas.height);
-  status.innerHTML = "Done";
-  document.getElementById("manual-button").style.display = "block";
+	console.log(width * height);
+	status.innerHTML = "Adjusting";
+	deltaX = canvas.width / 2 - faceCenterX;
+	deltaY = canvas.height / 2 - faceCenterY;
+	ctx.clearRect(0, 0, canvas.width, canvas.height);
+	// ctx.drawImage(bg, 0, 0, canvas.width, canvas.height * 1.05);
+	ctx.drawImage(
+		Im,
+		deltaX,
+		deltaY,
+		canvas.width,
+		canvas.width * (Im.height / Im.width)
+	);
+	ctx.drawImage(frame, 0, 0, canvas.width, canvas.height);
+	status.innerHTML = "Done";
+	document.getElementById("manual-button").style.display = "block";
 }
 
 load();
 
 function manual() {
-  heightSlider.style.display = "block";
-  widthSlider.style.display = "block";
-  render();
+	heightSlider.style.display = "block";
+	widthSlider.style.display = "block";
+	render();
 }
 
 heightSlider.oninput = function () {
-  dY = parseFloat(this.value);
-  deltaY = dY;
+	dY = parseFloat(this.value);
+	deltaY = dY;
 };
 widthSlider.oninput = function () {
-  dX = parseFloat(this.value);
-  deltaX = dX;
+	dX = parseFloat(this.value);
+	deltaX = dX;
 };
 
 function render() {
-  requestAnimationFrame(render);
-  ctx.drawImage(
-    Im,
-    deltaX,
-    deltaY,
-    canvas.width,
-    canvas.width * (Im.height / Im.width)
-  );
-  // console.log(deltaX, deltaY);
-  ctx.drawImage(frame, 0, 0, canvas.width, canvas.height * 1.05);
-  setTimeout(() => {
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-  }, 100);
+	requestAnimationFrame(render);
+	ctx.drawImage(
+		Im,
+		deltaX,
+		deltaY,
+		canvas.width,
+		canvas.width * (Im.height / Im.width)
+	);
+	// console.log(deltaX, deltaY);
+	ctx.drawImage(frame, 0, 0, canvas.width, canvas.height * 1.05);
+	setTimeout(() => {
+		ctx.clearRect(0, 0, canvas.width, canvas.height);
+	}, 100);
 }
