1d14984233de3b691ef06ce489d664e8fe337c4d
suyashsonawane <suyashsonawane007@gmail.com>
2020-10-21 22:43:13 +0530

    sync

--- badgemodule/src/components/BadgeModule.js
+++ badgemodule/src/components/BadgeModule.js
@@ -173,7 +173,7 @@ const BadgeModule = () => {
             </div>
           )}
 
-          <canvas id="badge" width="500" height="500"></canvas>
+          <canvas id="badge" width="1000" height="1000"></canvas>
       {file && (
         <img
         src={file}
@@ -193,14 +193,16 @@ const BadgeModule = () => {
           <img src={dumb_face} id="dumb-face" alt="" hidden />
           
 
-          {success && <div className="download-fab">
+          {success && 
             <a download="myBadge.jpg" href={success}>
+            <div className="download-fab">
               <svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 0 24 24" width="24" fill="#fff">
               <path d="M0 0h24v24H0V0z" fill="none"></path>
               <path d="M16.59 9H15V4c0-.55-.45-1-1-1h-4c-.55 0-1 .45-1 1v5H7.41c-.89 0-1.34 1.08-.71 1.71l4.59 4.59c.39.39 1.02.39 1.41 0l4.59-4.59c.63-.63.19-1.71-.7-1.71zM5 19c0 .55.45 1 1 1h12c.55 0 1-.45 1-1s-.45-1-1-1H6c-.55 0-1 .45-1 1z"></path>
-              </svg>
-              </a>
-          </div>}
+            </svg>
+            </div>
+            </a>
+          }
 
           
       </div>
