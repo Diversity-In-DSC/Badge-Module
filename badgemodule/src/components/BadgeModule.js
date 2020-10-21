a7c6b35733ddfa7ed4f7824abc64d19681b0e60c
suyashsonawane <suyashsonawane007@gmail.com>
2020-10-22 00:03:30 +0530

    new features

--- badgemodule/src/components/BadgeModule.js
+++ badgemodule/src/components/BadgeModule.js
@@ -1,9 +1,12 @@
 import React, { useState } from "react";
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import "@tensorflow/tfjs-backend-cpu";
 import ReactCrop from "react-image-crop";
 import "react-image-crop/dist/ReactCrop.css";
-import frame from "../Attendee-at-light.png";
+import frame from "../frame-light.png";
 import dumb_face from "../dumb-face.png";
+import logo from "../logo.png"
 
 const blazeface = require("@tensorflow-models/blazeface");
 
@@ -16,46 +19,54 @@ const BadgeModule = () => {
   const [file, setFile] = useState(null);
   const [crop, setCrop] = useState(null);
   const [image, setImage] = useState(null);
+  const [canvasDim, setCanvasDim] = useState({width:1000,height:1000});
   let model;
   load();
 
   async function load() {
+    
     model = await blazeface.load();
     setStatus("Model Loaded");
 
     const style = 'font-weight: bold; font-size: 30px;color: yellow; text-shadow: 3px 3px 0 rgb(217,31,38) ;padding : 50px ';
 
     console.log('%c Hi You have found an easter egg 🥚, now find a way to report this to us!', style);
-    
-    
-    if (!loaded) {
-      
+        
+    if (!loaded) {      
       let badge = document.getElementById("badge");
       let badgectx = badge.getContext("2d");
       let frame = document.getElementById("frame");
       let dumb_face = document.getElementById("dumb-face");
       badgectx.drawImage(dumb_face, 0, 0, badge.width, badge.height);
       badgectx.drawImage(frame, 0, 0, badge.width, badge.height);
+      
     }
     setLoaded(true);
-    
   }
 
   function handleFile(e) {
     setFile(URL.createObjectURL(e.target.files[0]));
-
     setTimeout(() => {
       run();
     }, 100);
   }
 
   async function run() {
+    let img = document.querySelector("#temp-image")
+  
+    if (img.naturalWidth < 500 || img.naturalHeight < 500)
+    {     
+      toast.warn("Low resolution image, resizing badge to low dimensions")
+      setCanvasDim({width:img.naturalWidth,height:img.naturalWidth})
+     
+      }
     let results = await model.estimateFaces(
-      document.querySelector("#temp-image"),
+      img,
       false
     );
     if (results[0])
     {
+      toast.success("shinning like a star ⭐")
       let result = results[0];
       let x = parseFloat(result.topLeft[0]) - 50;
       let y = parseFloat(result.topLeft[1]) - 50;
@@ -64,14 +75,14 @@ const BadgeModule = () => {
         x: x,
         y: y,
         aspect: 1,
-        width: 200,
-        height: 200,
+        width: 300,
+        height: 300,
       });
 
       setReady(true);
     }
     else {
-      alert("Can't detect your face")
+      toast.warn("Can't detect your face 😞")
       setCrop({
         x: 0,
         y: 0,
@@ -121,6 +132,7 @@ const BadgeModule = () => {
     };
     img.src = canvas.toDataURL("image/jpeg");
 
+    toast.success("psst, you can download badge from here ", { position: "bottom-right",autoClose: 3000 })
   };
 
   function uploadImage() {
@@ -130,9 +142,21 @@ const BadgeModule = () => {
 
   return (
     <>
+         <ToastContainer
+        position="top-center"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        />
       <header>DSC WOW Badge Maker</header>
 
       <div className="main-container">
+        
         <div className="input-panel">
 
           <div className="input">
@@ -140,25 +164,23 @@ const BadgeModule = () => {
             <span>
             <button className="button" onClick={uploadImage}>Upload Image</button>
               {showLoader && <div className="lds-ring"><div></div><div></div><div></div><div></div></div>}
-
-
             </span>
             {loaded && <input type="file" accept="image/*" onChange={handleFile} className="profile-input" hidden />}
+            {/* <h4>Status : {status}</h4> */}
+            <h5>
+              An AI Powered Badge Maker to automatically suggest crop region depending on the face location. <br/> <br />
+              Powered by Tensorflow.js <br /> <br />
+              Build with ❤️ by DSC WOW Team
+
+            </h5>
           </div>
 
-
-          <h4>Status : {status}</h4>
-          <h5>
-            An AI Powered Badge Maker to automatically suggest crop region depending on the face location. <br/> <br />
-            Powered by Tensorflow.js <br /> <br />
-            Build with ❤️ by DSC WOW Team
-
-          </h5>
+          <img src={logo} className="logo" alt="DSC WOW Logo"/>
           
         </div>
 
         <div className="preview-panel">     
-
+     
           {ready && (
             <div>
               <div style={{ maxWidth: "450px", width: "80vw" }}>
@@ -173,7 +195,7 @@ const BadgeModule = () => {
             </div>
           )}
 
-          <canvas id="badge" width="1000" height="1000"></canvas>
+          <canvas id="badge" width={canvasDim.width} height={canvasDim.height}></canvas>
       {file && (
         <img
         src={file}
@@ -206,15 +228,7 @@ const BadgeModule = () => {
 
           
       </div>
-
-
       </div>
-
-
-
-
-
-    
     </>
   );
 };
