af9c6d49e1e1ff89a3de08bc5d86a233313978cf
suyashsonawane <suyashsonawane007@gmail.com>
2020-11-13 20:28:36 +0530

    changes for github pages

---
+++ docs/precache-manifest.4d7ce8bf8939594b2078fac5deac8697.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ee439a8b0fe55ab21ec5ac824518a326",
+    "url": "./index.html"
+  },
+  {
+    "revision": "b7a98533541b84afebf1",
+    "url": "./static/css/2.3743f5bf.chunk.css"
+  },
+  {
+    "revision": "7d8d35e1b3302e361159",
+    "url": "./static/css/main.ad6dca1c.chunk.css"
+  },
+  {
+    "revision": "b7a98533541b84afebf1",
+    "url": "./static/js/2.6ae605d8.chunk.js"
+  },
+  {
+    "revision": "611eb6586a38a1198fe7a7e94fdf6e58",
+    "url": "./static/js/2.6ae605d8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7d8d35e1b3302e361159",
+    "url": "./static/js/main.7bd8a66f.chunk.js"
+  },
+  {
+    "revision": "29d00fcca067495c072c",
+    "url": "./static/js/runtime-main.23f2cfe0.js"
+  },
+  {
+    "revision": "78a942d46bd8783921d30a6e7163f696",
+    "url": "./static/media/dsc wow F5F5F5.78a942d4.gif"
+  },
+  {
+    "revision": "0caa4188171f398ad2dfe880d7a81942",
+    "url": "./static/media/frame-light.0caa4188.png"
+  }
+]);
